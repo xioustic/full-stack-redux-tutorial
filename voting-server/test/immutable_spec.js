329c197eadd93691e88101b13488067cfc9335e3
xioustic <xioustic@gmail.com>
2016-02-17 15:35:26 -0700

    added Immutability test for Map

--- voting-server/test/immutable_spec.js
+++ voting-server/test/immutable_spec.js
@@ -1,5 +1,5 @@
 import {expect} from 'chai';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 describe('immutability', () => {
 
@@ -40,6 +40,40 @@ describe('immutability', () => {
       ));
     });
     
-  })
+  });
+
+  describe('a tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable (using set and get)', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+
+    });
+
+  });
 
 });
