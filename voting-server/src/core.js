0e9fea2e2cf130dd8c044e8a2fd85e4f9b7c5a74
xioustic <xioustic@gmail.com>
2016-02-17 16:32:13 -0700

    added vote function plus tests

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -11,3 +11,15 @@ export function next(state) {
     entries: entries.skip(2)
   });
 }
+
+export function vote(state, entry) {
+  // reach into the nested data structure path ['vote', 'tally', entry]
+  // apply this function there
+  // if any keys missing along the path, create new maps in their place
+  // if the value at the end is missing, initialize with 0 before applying function
+  return state.updateIn(
+    ['vote','tally', entry],
+    0,
+    v => v + 1
+  )
+}
