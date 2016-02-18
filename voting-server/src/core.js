a061726062b4a0a3a5af6b9fe0cd42f0b03fbcac
xioustic <xioustic@gmail.com>
2016-02-17 17:11:24 -0700

    added determining winner in next() + tests + comments

--- voting-server/src/core.js
+++ voting-server/src/core.js
@@ -1,12 +1,14 @@
 import {List,Map} from 'immutable';
 
 function getWinners(vote) {
+  // if vote state does not exist, no winner(s)
   if (!vote) return [];
   const [a,b] = vote.get('pair');
   const aVotes = vote.getIn(['tally', a], 0);
   const bVotes = vote.getIn(['tally', b], 0);
   if    (aVotes > bVotes) return [a];
   if    (bVotes > aVotes) return [b];
+  // if tie, return both
   else                    return [a,b];
 }
 
@@ -15,11 +17,23 @@ export function setEntries(state, entries) {
 }
 
 export function next(state) {
+  // get the entries from state and try to determine the winner
+  // from the vote state (which may or may not exist)
   const entries = state.get('entries')
                        .concat(getWinners(state.get('vote')));
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
+
+  // if only one entry remains, it must be the winner
+  if (entries.size === 1) {
+    return state.remove('vote')
+                .remove('entries')
+                .set('winner', entries.first());
+  } else {
+    // pluck a pair of entries for voting and remove them from the
+    // state's entries
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    }
   });
 }
 
