808d89bb61ef57d3eba647dc6afda097ad442003
xioustic <xioustic@gmail.com>
2016-02-17 18:53:28 -0700

    added initial sample data when starting server

--- voting-server/index.js
+++ voting-server/index.js
@@ -4,3 +4,10 @@ import startServer from './src/server';
 export const store = makeStore();
 startServer(store);
 
+// set initial data
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+// push into the voting stage
+store.dispatch({type: 'NEXT'});
