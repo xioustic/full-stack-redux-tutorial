461e80f460cdcc480cd4b18e280246b70d33e090
xioustic <xioustic@gmail.com>
2016-02-18 14:41:17 -0700

    add redux, create store, dispatch initial store data

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
 require('./style.css');
+
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
 
 const routes = (
   <Route component={App}>
