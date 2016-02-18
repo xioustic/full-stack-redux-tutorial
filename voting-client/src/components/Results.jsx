effc7de55dc472b5b7085b9377dd327472c26f92
xioustic <xioustic@gmail.com>
2016-02-18 12:36:11 -0700

    added rendering Winner for Results component, plus tests

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -13,7 +14,10 @@ export default React.createClass({
     return 0;
   },
   render: function() {
-    return (
+    return this.props.winner ?
+    (
+      <Winner ref="winner" winner={this.props.winner} />
+    ) : (
       <div className="results">
         <div className="tally">
           {this.getPair().map(entry =>
