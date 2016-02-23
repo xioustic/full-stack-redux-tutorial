34af22271e1b5875a327a43367162c656b1294a3
xioustic <xioustic@gmail.com>
2016-02-23 13:38:17 -0700

    create socket.io connection in index.jsx

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {Router, Route} from 'react-router';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
@@ -20,6 +21,8 @@ store.dispatch({
     }
   }
 });
+
+const socket = io('ws://localhost:8090');
 
 const routes = (
   <Route component={App}>
