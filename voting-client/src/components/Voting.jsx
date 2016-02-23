d39b332167717cdf7cc2d9409026d45ad523df52
xioustic <xioustic@gmail.com>
2016-02-23 14:03:23 -0700

    connect action creators to the voting container

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -3,6 +3,7 @@ import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {connect} from 'react-redux';
 import Winner from './Winner';
 import Vote from './Vote';
+import * as actionCreators from '../action_creators';
 
 // pure, aka "dumb" version of the component
 export const Voting = React.createClass({
@@ -27,4 +28,4 @@ function mapStateToProps(state) {
 }
 
 // connect, aka "smart" version of the component
-export const VotingContainer = connect(mapStateToProps)(Voting);
+export const VotingContainer = connect(mapStateToProps, actionCreators)(Voting);
