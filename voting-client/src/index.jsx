5b4879bb7deeb99f820f672bccaa6b948bcbc315
xioustic <xioustic@gmail.com>
2016-02-23 13:44:12 -0700

    remove mock store initial state

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -12,15 +12,6 @@ import {ResultsContainer} from './components/Results';
 require('./style.css');
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
